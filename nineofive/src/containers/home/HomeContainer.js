0b09f156e35a6bc15f9f65c50c6f11c6047a4df0
HyunwooKim <hyunwoo.developer@gmail.com>
2021-09-26 15:54:02 +0900

    엔드포인트 변경

--- nineofive/src/containers/home/HomeContainer.js
+++ nineofive/src/containers/home/HomeContainer.js
@@ -1,26 +1,26 @@
 import React, { useEffect, useState } from "react";
 import HomeComponent from "../../components/home/HomeComponent";
-export const baseURL = "http://localhost:3000";
+export const baseURL =
+    "http://ec2-15-164-61-24.ap-northeast-2.compute.amazonaws.com:3000";
 
 export default function HomeContainer() {
-  const [isLogin, setIsLogin] = useState(false);
-  const [msg, setMsg] = useState("환영합니다.");
-  const getUserEmail = sessionStorage.getItem("user_email");
-  const baseURL = "http://localhost:3000";
+    const [isLogin, setIsLogin] = useState(false);
+    const [msg, setMsg] = useState("환영합니다.");
+    const getUserEmail = sessionStorage.getItem("user_email");
 
-  useEffect(() => {
-    if (getUserEmail === null) {
-    } else {
-      setIsLogin(true);
-      console.log(isLogin);
-      setMsg(getUserEmail + "님 환영합니다.🎉");
-    }
-  });
+    useEffect(() => {
+        if (getUserEmail === null) {
+        } else {
+            setIsLogin(true);
+            console.log(isLogin);
+            setMsg(getUserEmail + "님 환영합니다.🎉");
+        }
+    });
 
-  const onLogout = () => {
-    sessionStorage.removeItem("user_email");
-    document.location.href = "/";
-  };
+    const onLogout = () => {
+        sessionStorage.removeItem("user_email");
+        document.location.href = "/";
+    };
 
-  return <HomeComponent msg={msg} onLogout={onLogout} />;
+    return <HomeComponent msg={msg} onLogout={onLogout} />;
 }
